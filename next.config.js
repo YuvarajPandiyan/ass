a180b70b04168c99eeb2d83fa14ecfcb8f0111ea
Yuvaraj Pandiyan <yuvayuvaraj060@gmail.com>
2021-10-30 13:42:59 +0530

    enabling CORS error fixed

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return [
       {
         // matching all routes
-        source: "/*",
+        source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
