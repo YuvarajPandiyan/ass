03168abcd6faa1d8f594da7ff623000bae1b9a74
Yuvaraj Pandiyan <yuvayuvaraj060@gmail.com>
2021-10-30 13:26:16 +0530

    enabling CORS all routes

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ module.exports = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        // matching all routes
+        source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
